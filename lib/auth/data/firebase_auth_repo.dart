f8affd9f82ae7086a352d9b5981b32dac5044490
mrnipundilshan <nipundilshan991@gmail.com>
2025-09-15 13:38:54 +0530

    login firebaseauth

--- lib/auth/data/firebase_auth_repo.dart
+++ lib/auth/data/firebase_auth_repo.dart
@@ -1,16 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:datingapp/auth/domain/entities/app_user.dart';
 import 'package:datingapp/auth/domain/repository/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthRepo implements AuthRepository {
+  // firebase auth instance
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // firebase firestore instance
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
   @override
-  Future<AppUser?> getCurrentUser() {
-    // TODO: implement getCurrentUser
-    throw UnimplementedError();
+  Future<AppUser?> loginWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      // attemt sign in
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      // fetch user document from firebase
+      DocumentSnapshot userDoc = await firebaseFirestore
+          .collection('users')
+          .doc(userCredential.user!.uid)
+          .get();
+
+      // App user
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: email,
+        name: userDoc['name'],
+      );
+
+      return user;
+    } catch (e) {
+      // catch any errors
+      Exception("Login failed: $e");
+    }
+    return null;
   }
 
   @override
-  Future<AppUser?> loginWithEmailAndPassword(String email, String password) {
-    // TODO: implement loginWithEmailAndPassword
+  Future<AppUser?> getCurrentUser() {
+    // TODO: implement getCurrentUser
     throw UnimplementedError();
   }
 
