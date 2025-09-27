4012d55a0f3ddf342d339e6da2154451631a06ae
mrnipundilshan <nipundilshan991@gmail.com>
2025-09-27 14:59:14 +0530

    nkd version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.datingapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+     ndkVersion = "29.0.14033849"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
