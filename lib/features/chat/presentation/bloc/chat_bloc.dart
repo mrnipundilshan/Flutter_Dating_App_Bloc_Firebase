44f636ed8e2c5d583944d6f0f8216902c71fe05e
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-11-15 23:53:11 +0530

    chat bloc

--- lib/features/chat/presentation/bloc/chat_bloc.dart
+++ lib/features/chat/presentation/bloc/chat_bloc.dart
@@ -1,13 +1,73 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:datingapp/features/chat/domain/entities/message.dart';
+import 'package:datingapp/features/chat/domain/repositories/chat_repositories.dart';
 import 'package:equatable/equatable.dart';
 
 part 'chat_event.dart';
 part 'chat_state.dart';
 
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
-  ChatBloc() : super(ChatInitial()) {
-    on<ChatEvent>((event, emit) {
-      // TODO: implement event handler
+  final ChatRepositories chatRepository;
+  Stream<List<Message>>? _messageStream;
+
+  ChatBloc(this.chatRepository) : super(ChatInitial()) {
+    on<LoadMessageEvent>(_onLoadMessages);
+
+    on<SendMessageEvent>(_onSendMessage);
+
+    on<MarkMessageSeenEvent>(_onMarkedMessageSeen);
+  }
+
+  FutureOr<void> _onLoadMessages(
+    LoadMessageEvent event,
+    Emitter<ChatState> emit,
+  ) {
+    emit(ChatLoading());
+
+    _messageStream = chatRepository.getMessages(
+      event.currentUserId,
+      event.peerId,
+    );
+
+    _messageStream!.listen((messages) {
+      add(_MessagesUpdatedEvent(messages));
     });
   }
+
+  FutureOr<void> _onSendMessage(
+    SendMessageEvent event,
+    Emitter<ChatState> emit,
+  ) async {
+    try {
+      await chatRepository.sendMessage(
+        event.senderId,
+        event.receiverId,
+        event.text,
+      );
+    } catch (e) {
+      emit(ChatError("Failed to send message."));
+    }
+  }
+
+  FutureOr<void> _onMarkedMessageSeen(
+    MarkMessageSeenEvent event,
+    Emitter<ChatState> emit,
+  ) async {
+    try {
+      await chatRepository.markMessageAsSeen(event.messageId, event.chatId);
+    } catch (e) {
+      emit(ChatError("Failed to mark message as seen."));
+    }
+  }
+}
+
+// private internal event
+class _MessagesUpdatedEvent extends ChatEvent {
+  final List<Message> messages;
+  const _MessagesUpdatedEvent(this.messages);
+
+  @override
+  List<Object> get props => [messages];
 }
