798580fae433ec47506050d8de44050db2da5206
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-11-15 20:13:00 +0530

    chat events

--- lib/features/chat/presentation/bloc/chat_event.dart
+++ lib/features/chat/presentation/bloc/chat_event.dart
@@ -6,3 +6,38 @@ sealed class ChatEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class LoadMessageEvent extends ChatEvent {
+  final String currentUserId;
+  final String peerId;
+
+  const LoadMessageEvent({required this.currentUserId, required this.peerId});
+
+  @override
+  List<Object> get props => [currentUserId, peerId];
+}
+
+class SendMessageEvent extends ChatEvent {
+  final String senderId;
+  final String receiverId;
+  final String text;
+
+  const SendMessageEvent({
+    required this.senderId,
+    required this.receiverId,
+    required this.text,
+  });
+
+  @override
+  List<Object> get props => [senderId, receiverId, text];
+}
+
+class MarkMessageSeenEvent extends ChatEvent {
+  final String chatId;
+  final String messageId;
+
+  const MarkMessageSeenEvent({required this.chatId, required this.messageId});
+
+  @override
+  List<Object> get props => [chatId, messageId];
+}
